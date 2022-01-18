9f63a803e056bdb741692ccbae1801c5596a6c4b
alex1504 <alex1504@163.com>
2022-01-18 10:23:20 +0800

    fix: rm doc

--- src/api/index.js
+++ src/api/index.js
@@ -1,14 +1,5 @@
 import { httpFetch } from '../util'
 
-/**
-    token: A-OceKHjP80h7bvq9TE5J0DrKazEw707laHo5jtm:8gEa39Pjhg_fO3cJNVcCkk8ZwBs=:eyJzY29wZSI6ImJsb2ciLCJkZWFkbGluZSI6MTY0MTU0NTQ2ODc1MX0=
-    id: WU_FILE_0
-    name: 测试.png
-    type: image/png
-    lastModifiedDate: Tue May 18 2021 11:45:30 GMT+0800 (中国标准时间)
-    size: 304048
-    file: (binary)
- */
 export function uploadQiniu(
   data = {
     file,
